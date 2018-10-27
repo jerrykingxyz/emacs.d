7c657a53ca1019d7e759e28444c25e5687bb1f1b
jerry <jerrykingxyz@gmail.com>
2018-10-27 23:42:45 +0800

    feat : company mode show numbers

--- lisp/init-company.el
+++ lisp/init-company.el
@@ -6,4 +6,11 @@
 
 (add-hook 'after-init-hook 'global-company-mode)
 
+(setq company-show-numbers t)
+;; 使用数字0-9完成选择
+(let ((map company-active-map))
+  (mapc (lambda (x) (define-key map (format "%d" x)
+                 `(lambda () (interactive) (company-complete-number ,x))))
+        (number-sequence 0 9)))
+
 (provide 'init-company)
