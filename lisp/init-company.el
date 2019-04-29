ff2180ddd23158f0ac8caef348254f4fd8388922
jerrykingxyz <jerrykingxyz@gmail.com>
2019-04-29 23:01:07 +0800

    clear: remove ycmd

--- lisp/init-company.el
+++ lisp/init-company.el
@@ -1,8 +1,5 @@
-(if (boundp 'ycmd-server-command)
-    (progn (require-package 'company-ycmd)
-           (company-ycmd-setup))
-  (progn (require-package 'company)
-         (require 'company)))
+(require-package 'company)
+(require 'company)
 
 (add-hook 'after-init-hook 'global-company-mode)
 
